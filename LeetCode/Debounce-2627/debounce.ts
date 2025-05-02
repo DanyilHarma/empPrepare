5e6aa3c1a529cd27eac2ab0216cd52becf8a8ee5
Danyil <danilagarma1999@mail.ru>
2025-05-02 18:46:14 +0300

    add debounce problem

--- LeetCode/Debounce-2627/debounce.ts
+++ LeetCode/Debounce-2627/debounce.ts
@@ -2,7 +2,6 @@ type F = (...args: number[]) => void;
 
 function debounce(fn: F, t: number): F {
     let timerId: ReturnType<typeof setTimeout> | null = null;
-
     return function (...args: number[]) {
         if (timerId !== null) {
             clearTimeout(timerId);
